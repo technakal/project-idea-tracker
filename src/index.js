a674b4b33eca02ae7fb86277fdac5810616eab62
Noel Keener <keenen1@adroot.phmic.com>
2019-04-20 11:11:27 -0500

    Add total tracking
    Added tracking for total completed by category and list.

--- src/index.js
+++ src/index.js
@@ -23,7 +23,9 @@ class Project {
   }
 
   createProjectElem() {
-    return `<li><input type="checkbox" id="${this.id}" ${this.completed ? "checked" : ""}/><label for="${this.id}">${this.text}</label></li>`
+    return `<li><input type="checkbox" id="${this.id}" ${
+      this.completed ? "checked" : ""
+    }/><label for="${this.id}">${this.text}</label></li>`;
   }
 }
 
@@ -47,33 +49,50 @@ class ProjectList {
   }
 
   getCategories() {
-    const categories = this.projects.map(project => project.category)
+    const categories = this.projects.map(project => project.category);
     return categories.filter((item, index) => categories.indexOf(item) >= index);
   }
 
+  getProjectsByCategory(category) {
+    return this.projects
+      .map(project => (project.category === category ? project : null))
+      .filter(project => project !== null);
+  }
+
+  getTotalProjects(list) {
+    return list.length;
+  }
+
   getTotalCompleted(list) {
-    const total = list.map(project => {
-      if (project.completed) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
-    return total.reduce((a, b) => a + b);
+    return list
+      .map(project => {
+        if (project.completed) {
+          return 1;
+        } else {
+          return 0;
+        }
+      })
+      .reduce((a, b) => a + b);
   }
 
   createCategoryElem(category) {
     const categoryArray = category.split(" ");
     const categoryId = categoryArray.join("-");
-    const categoryName = categoryArray.map(word => word[0].toUpperCase() + word.slice(1, word.length)).join(" ");
-    return `<h3>${categoryName}</h3><ul id="${categoryId}"></ul>`;
+    const categoryName = categoryArray
+      .map(word => word[0].toUpperCase() + word.slice(1, word.length))
+      .join(" ");
+    return `<div class="checklist"><div class="checklist-header"><h3 class="checklist-title">${categoryName}</h3><span class="checklist-total">${this.getTotalCompleted(
+      this.projects
+    )}/${this.getTotalProjects(
+      this.getProjectsByCategory(category)
+    )}</span></div><ul class="collapsed" id="${categoryId}"></ul></div>`;
   }
 
   createCategoryView(categories) {
     return categories.map(category => this.createCategoryElem(category)).join("");
   }
 
-  createTotalView(total) {
+  createTotalView(list) {
     // returns the DOM representation of the total
     // can be used to return the total completed
     // can be used to return the total completed for a single category
@@ -123,7 +142,9 @@ class App {
   }
 
   findProjectIndex(listName) {
-    const projectList = this.projectLists.find(projectList => projectList.listName.toLowerCase() === listName.toLowerCase());
+    const projectList = this.projectLists.find(
+      projectList => projectList.listName.toLowerCase() === listName.toLowerCase()
+    );
     return this.projectLists.indexOf(projectList);
   }
 
@@ -140,12 +161,19 @@ class App {
     // retrieves state information from localStorage or the data.json file, assuming I can get that to work.
   }
 
+  toggleCollapsed(elem) {
+    if (elem.className === "collapsed") {
+      return this.updateClassName(elem, "open");
+    }
+    return this.updateClassName(elem, "collapsed");
+  }
+
   updateDomLocation(html, location) {
     document.querySelector(location).innerHTML = html;
   }
 
-  updateClassNames(elem, newClass) {
-    // sets the class of the elem to newClass
+  updateClassName(elem, newClass) {
+    return (elem.className = newClass);
   }
 }
 
@@ -161,26 +189,45 @@ const initializer = {
 
   createApp() {
     const app = new App();
-    app.retrieveDataSource(app._baseUrl)
-      .then(data => {
-        const projectLists = data.projectLists.map(list => this.createProjectList(list));
-        app.projectLists = projectLists;
-        app.currentList = data.currentList;
-        const currentListContent = app.projectLists[app.findProjectIndex(app.currentList)];
-        const categories = currentListContent.getCategories();
-        const categoryHtml = currentListContent.createCategoryView(categories);
-        app.updateDomLocation(categoryHtml, '.checklist');
-        categories.map(category => {
-          const projectHtml = currentListContent.projects.map(project => {
-            return project.category === category ? project.createProjectElem() : null;
-          }).filter(project => project !== null).join("");
-          app.updateDomLocation(projectHtml, `#${category.split(" ").join("-")}`)
-        });
-        app.updateDomLocation(`<a href="${currentListContent.source}">${app.currentList[0].toUpperCase() + app.currentList.slice(1, app.currentList.length)}</a>`, "#source");
-        return app;
-      });
-  }
-}
+    app.retrieveDataSource(app._baseUrl).then(data => {
+      const projectLists = data.projectLists.map(list => this.createProjectList(list));
+      app.projectLists = projectLists;
+      app.currentList = data.currentList;
+      const currentListContent = app.projectLists[app.findProjectIndex(app.currentList)];
 
+      app.totalCompleted = currentListContent.getTotalCompleted(currentListContent.projects);
+      app.updateDomLocation(
+        `${app.totalCompleted}/${currentListContent.getTotalProjects(currentListContent.projects)}`,
+        "#totalCompleted"
+      );
+      const categories = currentListContent.getCategories();
+      const categoryHtml = currentListContent.createCategoryView(categories);
+      app.updateDomLocation(categoryHtml, "#checklists");
+      categories.map(category => {
+        const projectHtml = currentListContent.projects
+          .map(project => {
+            return project.category === category ? project.createProjectElem() : null;
+          })
+          .filter(project => project !== null)
+          .join("");
+        app.updateDomLocation(projectHtml, `#${category.split(" ").join("-")}`);
+      });
+      app.updateDomLocation(
+        `<a href="${currentListContent.source}">${app.currentList[0].toUpperCase() +
+          app.currentList.slice(1, app.currentList.length)}</a>`,
+        "#source"
+      );
+      const checklists = document.querySelectorAll(".checklist");
+      checklists.forEach(item =>
+        item.addEventListener("click", e => {
+          if (e.target.tagName === "H3") {
+            app.toggleCollapsed(e.target.parentNode.parentNode.querySelector("ul"));
+          }
+        })
+      );
+      return app;
+    });
+  }
+};
 
 const app = initializer.createApp();
